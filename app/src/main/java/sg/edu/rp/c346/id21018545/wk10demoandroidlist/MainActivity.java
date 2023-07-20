fe8043bcb91740560d1b791d66657c1c908f861d
Shakir21018545 <21018545@myrp.edu.sg>
2023-07-20 09:42:42 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21018545/wk10demoandroidlist/MainActivity.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.id21018545.wk10demoandroidlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+
+    ArrayList<String> items = new ArrayList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv = findViewById(R.id.lv);
+
+        items.add("Pie: 9.0");
+        items.add("Oreo: 8.0-8.1");
+        items.add("Nougat: 7.0-7.1.2");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
+
+        lv.setAdapter(adapter);
+    }
+
+}
